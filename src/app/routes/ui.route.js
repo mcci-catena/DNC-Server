2b47af0d791f5fd03d70d16e890844bf982c18ca
sivaprakash <sveluthambi@mcci.com>
2021-08-13 11:28:14 +0530

    added user model routes

--- src/app/routes/ui.route.js
+++ src/app/routes/ui.route.js
@@ -2,10 +2,10 @@ const uictrl = require('../controllers/ui.controller.js');
 
 module.exports = (app) => {
     // General signup request from UI reach here
-    app.post('/signup', uictrl.signup);
+    app.get('/signup', uictrl.signup);
     
     // Admin Email configuration
-    app.post('/saecon', uictrl.updtaemail);
+    // app.post('/saecon', uictrl.updtaemail);
     
     // Admin signup
     app.post('/asignup', uictrl.asignup);
@@ -14,12 +14,20 @@ module.exports = (app) => {
     app.post('/usignup', uictrl.usignup);
 
     // Send OTP through Email
-    // app.post('/send-otp', uictrl.sendOtp);
-    app.post('/send-otp', uictrl.checkSendOtp);
-
-    // Authendicate User input
-    app.post('/chkmoa', uictrl.verifyAuth);
-
-    // Test
-    app.post('/test-api', uictrl.testApi)
+    app.post('/send-otp', uictrl.sendOtp);
+	
+	// List user details
+	app.get('/list-user', uictrl.listuser);
+	
+	// Update user details
+	app.put('/update-user/:uname', uictrl.updateuser);
+	
+	// Delete user details
+	app.delete('/delete-user/:uname', uictrl.deleteuser);
+	
+	// Forgot password
+	app.put('/reset-pwd', uictrl.forgotpwd);
+	
+	// Login 
+	app.post('/login', uictrl.uiLogin);
 }
